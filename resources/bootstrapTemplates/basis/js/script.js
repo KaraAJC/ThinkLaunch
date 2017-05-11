da643a129bc90b309bc4b3a4c096b0361e91e4b1
karaAJC <KaraAJC@gmail.com>
2017-05-10 20:27:45 -0500

    update bootstrap resources for session 6

---
+++ resources/bootstrapTemplates/basis/js/script.js
@@ -0,0 +1,17 @@
+// STEP ZERO! Make sure you have the URL setup the way you need it to be
+  let myUrl = 'https://api.github.com/users/catrina'
+
+// STEP ONE! let’s fetch an endpoint, which creates a promise
+  fetch(myUrl)
+
+// STEP TWO! fetch gives a blob, from which we pull the JSON data, lets make sure it's formatted to JSON
+      .then(data => data.json())
+
+// STEP THREE! Let's look at the Data we've gotten from the API
+      .then(data => {
+          console.log(data);
+
+// STEP FOUR! DO SOME THINGS TO THE DATA!
+
+      }
+  );
